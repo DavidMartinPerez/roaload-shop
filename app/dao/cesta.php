813ec240288ff6cc58d0965a3bffec1bc3740ec6
David Martín Pérez <davidmartinperez1@gmail.com>
2018-05-31 08:53:22 +0200

    Update cesta.php

--- app/dao/cesta.php
+++ app/dao/cesta.php
@@ -1,30 +1,67 @@
 <?php
+    //TODO: Probar que todo funcione
+    //FIXME: A lo mejor hay errores con la exportacion del array de objectos.
+    //TODO: mirar la otra vista que estará preparada para mostrar el objecto y funcionar directamente.
+	class ProductoCesta{ // Objecto con los datos que se mostrarán en cesta
+		public $idProducto; //Id del producto
+		public $cantidad; //Cantidad de articulos que a seleccionado
+		public $nombreJuego; // Nombre del Articulo
+		public $nombreEdicion; // Nombre de la edicion
+		public $nombrePlataforma; // Nombre de la plataforma
+		public $nombreDistribuidora; // Nombre de la distribuidora
+		public $precioPorArticulo; // Precio del producto
 
+		public function __construct($id,$cantidad,$nombreJuego,$nombreEdicion,$nombrePlataforma,$nombreDistribuidora,$precio){
+			this->$idProducto = $id;
+			this->$cantidad = $cantidad;
+			this->$nombreJuego = $nombreJuego;
+			this->$nombreEdicion = $nombreEdicion;
+			this->$nombrePlataforma = $nombrePlataforma;
+			this->$nombreDistribuidora = $nombreDistribuidora;
+			this->$precio = $precio;
+		}
+	}
     require 'conexion.php';
     class Cesta {
         //######### Obtener todos los productos de la cesta ##############
         public function obtenerTodosProductos($arrayCarrito){
+			// Se obtienen los datos por la id y se añaden a un objecto.
             global $bd;
-
-            $i = 0;
+			$ArrayObjectoProdutos = [];
+			$i = 0;
+			$productos = [];
             $ArrayLength = count($arrayCarrito);
             while($i < $ArrayLength){
-                $IdProductos = "idVersion = ";
-                $IdProductos .= $arrayCarrito[0]["id"];
-                $i++;
-            }
-            //SQL Para recuperar todos los Juegos y Consolas que esten habilitados!
-            $sqlDatosCesta = "SELECT version.idVersion, juego.nombreJuego, ed.nombreEdicion, ptl.nombrePlataforma, version.precio, version.stock, version.fechaSalida, dis.nombreDistribuidora,version.img
-            FROM videojuego juego, versionjuego version, edicion ed , plataforma ptl, distribuidora dis
-            WHERE version.idEdicion = ed.idEdicion AND version.idJuego = juego.idJuego AND version.idPlataforma = ptl.idPlataforma AND version.idDistribuidora = dis.idDistribuidora AND version.activo = 1
-            AND ($IdProductos)";
-            //FIXME: Se perderan los productos la cantidad de cada producto
-            // TODO: CREARLO COMO UN JSON
-            //$reg = $bd->query($sqlDatosCesta);
-            //$bd->close();
+				
+				//SQL Para recuperar todos los Juegos y Consolas que esten habilitados!
+				$sqlDatosCesta = "SELECT version.idVersion, juego.nombreJuego, ed.nombreEdicion, ptl.nombrePlataforma, version.precio, version.stock, version.fechaSalida, dis.nombreDistribuidora,version.img
+				FROM videojuego juego, versionjuego version, edicion ed , plataforma ptl, distribuidora dis
+				WHERE version.idEdicion = ed.idEdicion AND version.idJuego = juego.idJuego AND version.idPlataforma = ptl.idPlataforma AND version.idDistribuidora = dis.idDistribuidora AND version.activo = 1
+				AND (idVersion = ". $arrayCarrito[$i]["id"] .")";
 
-            return $sqlDatosCesta;
+				$reg = $bd->query($sqlDatosCesta);
+				$bd->close();
+				$datos = mysqli_fetch_assoc($reg);
+
+				//Creamos los datos del objecto.
+				$IdProducto = $arrayCarrito[$i]["id"];
+				$cantidad = $arrayCarrito[$i]["cantidad"];
+				$nombreJuego = $datos["nombreJuego"];
+				$nombreEdicion = $datos["nombreEdicion"];
+				$nombrePlataforma = $datos["nombrePlataforma"]
+				$nombreDistribuidora = $datos["nombreDistribuidora"];
+				$precio = $datos["precio"];
+				//Creamos el objecto con los datos.
+				$objecto = new ProductoCesta($IdProducto,$cantidad,$nombreJuego,$nombreEdicion,$nombrePlataforma,$nombreDistribuidora,$precio);
+				// Lo añadimos al array de objectos que devolveremos.
+				array.push($ArrayObjectoProdutos, $objecto);
+				//aumnetamos la variable para el siguiente producto.
+				$i++;
+            }
+            
+            return $ArrayObjectoProdutos;
         }
         //########## /Obtener todos los productos de la cesta ##################
     } //class Producto
 ?>
+
